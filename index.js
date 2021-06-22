0201a1e8f0755fbdc1c970abb01a8f1cfee859c3
satyam2201 <76933787+satyam2201@users.noreply.github.com>
2021-06-23 01:08:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+var n = Math.random();
+n = n * 100 + 1;
+var randomNum = Math.floor(n);
+// alert(randomNum)
+
+var count = 0;
+function myFunction() {
+  ++count;
+  
+}
+
+document.getElementById("mybtn").addEventListener("click", function () {
+  var inputNum = document.getElementById("number").value;
+
+  if (inputNum > randomNum) {
+    document.getElementById("demo").innerHTML =
+      "NO, Guess smaller number than "+inputNum ;
+  } else if (inputNum < randomNum) {
+    document.getElementById("demo").innerHTML =
+      "NO,Guess greater number than "+inputNum;
+  } else if ((randomNum = inputNum)) {
+    document.getElementById("demo").innerHTML =
+      "You passed in " + ++count + " attempts 🎉";
+      var winmusic = new Audio("win.wav");
+            winmusic.play();
+  }
+  
+});
+
+document.getElementById("Restart").addEventListener("click", function () {
+  location.reload();
+});
